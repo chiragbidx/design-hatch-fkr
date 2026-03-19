ca4d16b4afc9b5dc1d68da7542b909b2edfcc576
chiragbidx <chirag@bidx.ai>
2026-03-20 00:59:30 +0530

    Navbar blue gradients

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -33,9 +33,9 @@ interface NavbarProps {
 export const Navbar = ({ isLoggedIn = false }: NavbarProps) => {
   const [isOpen, setIsOpen] = React.useState(false);
   return (
-    <header className="shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-secondary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
+    <header className="shadow-blue-lg shadow-inner bg-opacity-15 w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border border-primary z-40 rounded-2xl flex justify-between items-center p-2 bg-card">
       <Link href="/" className="font-bold text-lg flex items-center">
-        <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
+        <ChevronsDown className="bg-gradient-to-tr from-primary via-blue-500 to-blue-700 border-primary rounded-lg w-9 h-9 mr-2 border text-white shadow-blue-lg" />
         {navbar.brandName}
       </Link>
       {/* <!-- Mobile --> */}
@@ -50,13 +50,13 @@ export const Navbar = ({ isLoggedIn = false }: NavbarProps) => {
 
           <SheetContent
             side="left"
-            className="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card border-secondary"
+            className="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card border-primary"
           >
             <div>
               <SheetHeader className="mb-4 ml-4">
                 <SheetTitle className="flex items-center">
                   <Link href="/" className="flex items-center">
-                    <ChevronsDown className="bg-gradient-to-tr border-secondary from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
+                    <ChevronsDown className="bg-gradient-to-tr from-primary via-blue-500 to-blue-700 border-primary rounded-lg w-9 h-9 mr-2 border text-white shadow-blue-lg" />
                     {navbar.brandName}
                   </Link>
                 </SheetTitle>
